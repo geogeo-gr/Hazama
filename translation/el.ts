6ed4762afd352c23d2398aa82e6a14ee98673d40
geogeo.gr <geogeo64@users.sourceforge.net>
2016-12-09 23:17:14 +0200

    Create el.ts
    Greek language file to v1.0.1

---
+++ translation/el.ts
@@ -0,0 +1,466 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el_GR">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <source>OK</source>
+        <translation>Εντάξει</translation>
+    </message>
+    <message>
+        <source>N/A</source>
+        <translation>Μη διαθέσιμα</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Μέγεθος</translation>
+    </message>
+    <message>
+        <source>Current diary book will be replaced with the backup!</source>
+        <translation>Το τρέχον βιβλίο ημερολογίου θα αντικατασταθεί με το αντίγραφο ασφαλείας!</translation>
+    </message>
+    <message>
+        <source>Retry</source>
+        <translation>Επανάληψη</translation>
+    </message>
+    <message>
+        <source>Install</source>
+        <translation>Εγκατάσταση</translation>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation>Σύνδεση...</translation>
+    </message>
+    <message>
+        <source>checking...</source>
+        <translation>Γίνεται έλεγχος...</translation>
+    </message>
+    <message>
+        <source>Failed to check update: %s</source>
+        <translation>Απέτυχε ο έλεγχος ενημερώσεων: %s</translation>
+    </message>
+    <message>
+        <source>Export Diary</source>
+        <translation>Εξαγωγή ημερολογίου</translation>
+    </message>
+    <message>
+        <source>Installing...</source>
+        <translation>Εγκατάσταση...</translation>
+    </message>
+    <message>
+        <source>Check update</source>
+        <translation>Έλεγχος ενημέρωσης</translation>
+    </message>
+    <message>
+        <source>New version: v%s</source>
+        <translation>Νέα έκδοση: v%s</translation>
+    </message>
+    <message>
+        <source>Ignore this version</source>
+        <translation>Παράβλεψη αυτής της έκδοσης</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Δημιουργός</translation>
+    </message>
+    <message>
+        <source>Export Failed</source>
+        <translation>Η εξαγωγή απέτυχε</translation>
+    </message>
+    <message>
+        <source>Plain Text (*.txt)</source>
+        <translation>Απλό κείμενο (*.txt)</translation>
+    </message>
+    <message>
+        <source>Restore backup</source>
+        <translation>Επαναφορά αντιγράφου ασφαλείας</translation>
+    </message>
+    <message>
+        <source>Succeeded (Restart needed for update to take effect)</source>
+        <translation>Επιτυχής (Απαιτείται επανεκκίνηση για εφαρμογή της ενημέρωσης)</translation>
+    </message>
+    <message>
+        <source>up-to-date</source>
+        <translation>ενημερωμένη</translation>
+    </message>
+    <message>
+        <source>Every &lt;b&gt;%s&lt;/b&gt; days a diary, from &lt;b&gt;%s&lt;/b&gt; to &lt;b&gt;%s&lt;/b&gt;</source>
+        <translation>Κάθε &lt;b&gt;%s&lt;/b&gt; ημέρες ένα ημερολόγιο, από &lt;b&gt;%s&lt;/b&gt; έως &lt;b&gt;%s&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>Ιστότοπος</translation>
+    </message>
+    <message>
+        <source>Failed to install update: %s</source>
+        <translation>Απέτυχε η εγκατάσταση της ενημέρωσης: %s</translation>
+    </message>
+</context>
+<context>
+    <name>TagList</name>
+    <message>
+        <source>All</source>
+        <translation>Όλες</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Μετονομασία</translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow</name>
+    <message>
+        <source>New</source>
+        <translation>Νέο</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <source>Hazama</source>
+        <translation>Δείγμα</translation>
+    </message>
+    <message>
+        <source>Tag List</source>
+        <translation>Λίστα ετικετών</translation>
+    </message>
+    <message>
+        <source>Sort By</source>
+        <translation>Ταξινόμηση κατά</translation>
+    </message>
+    <message>
+        <source>Heat Map</source>
+        <translation>Χάρτης θερμότητας</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ρυθμίσεις</translation>
+    </message>
+</context>
+<context>
+    <name>NTextEdit</name>
+    <message>
+        <source>Bold</source>
+        <translation>Έντονα</translation>
+    </message>
+    <message>
+        <source>Strike out</source>
+        <translation>Διακριτή διαγραφή</translation>
+    </message>
+    <message>
+        <source>Clear format</source>
+        <translation>Απαλοιφή μορφοποίησης</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Μορφοποίηση</translation>
+    </message>
+    <message>
+        <source>Italic</source>
+        <translation>Πλάγια γραφή</translation>
+    </message>
+    <message>
+        <source>Underline</source>
+        <translation>Υπογράμμιση</translation>
+    </message>
+    <message>
+        <source>Highlight</source>
+        <translation>Επισήμανση</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Date</source>
+        <translation>Ημερομηνία</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Τίτλο</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Μήκος</translation>
+    </message>
+    <message>
+        <source>Descending</source>
+        <translation>Φθίνουσα σειρά</translation>
+    </message>
+    <message>
+        <source>Ascending</source>
+        <translation>Αύξουσα σειρά</translation>
+    </message>
+    <message>
+        <source>%i diaries</source>
+        <translation>%i ημερολόγια</translation>
+    </message>
+    <message>
+        <source>loading...</source>
+        <translation>φόρτωση...</translation>
+    </message>
+</context>
+<context>
+    <name>NikkiList</name>
+    <message>
+        <source>Edit</source>
+        <translation>Επεξεργασία</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>Τυχαία</translation>
+    </message>
+    <message>
+        <source>Delete diaries</source>
+        <translation>Διαγραφή ημερολογίων</translation>
+    </message>
+    <message>
+        <source>Selected diaries will be deleted permanently!</source>
+        <translation>Τα επιλεγμένα ημερολόγια θα διαγραφούν οριστικά!</translation>
+    </message>
+</context>
+<context>
+    <name>configDialog</name>
+    <message>
+        <source>Text</source>
+        <translation>Κείμενο</translation>
+    </message>
+    <message>
+        <source>Diary</source>
+        <translation>Ημερολόγιο</translation>
+    </message>
+    <message>
+        <source>Fonts</source>
+        <translation>Γραμματοσειρές</translation>
+    </message>
+    <message>
+        <source>Lists</source>
+        <translation>Λίστες</translation>
+    </message>
+    <message>
+        <source>Text:</source>
+        <translation>Κείμενο:</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Θέμα</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Τίτλος</translation>
+    </message>
+    <message>
+        <source>About Hazama</source>
+        <translation>Περί Hazama</translation>
+    </message>
+    <message>
+        <source>Enable read-only mode for diaries older than 3 days</source>
+        <translation>Ενεργοποίηση λειτουργίας μόνο για ανάγνωση για ημερολόγια άνω των 3 ημερών</translation>
+    </message>
+    <message>
+        <source>Backup every day and keep each one for a week.
+</source>
+        <translation>Αντίγραφα ασφαλείας κάθε μέρα και διατήρησή τους για μια εβδομάδα.
+</translation>
+    </message>
+    <message>
+        <source>Select to restore</source>
+        <translation>Επιλογή για επαναφορά</translation>
+    </message>
+    <message>
+        <source>Color Scheme:</source>
+        <translation>Συνδυασμός χρωμάτων:</translation>
+    </message>
+    <message>
+        <source>Show counts in tag list</source>
+        <translation>Εμφάνιση μετρητών στη λίστα ετικετών</translation>
+    </message>
+    <message>
+        <source>Backup</source>
+        <translation>Αντίγραφα ασφαλείας</translation>
+    </message>
+    <message>
+        <source>Editor</source>
+        <translation>Επεξεργαστής</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Εξαγωγή</translation>
+    </message>
+    <message>
+        <source>Language:</source>
+        <translation>Γλώσσα / Lang:</translation>
+    </message>
+    <message>
+        <source>Theme:</source>
+        <translation>Θέμα:</translation>
+    </message>
+    <message>
+        <source>Title:</source>
+        <translation>Τίτλος:</translation>
+    </message>
+    <message>
+        <source>Initial cursor position:</source>
+        <translation>Αρχική θέση δρομέα:</translation>
+    </message>
+    <message>
+        <source>Auto indent</source>
+        <translation>Αυτόματη εσοχή</translation>
+    </message>
+    <message>
+        <source>Check for update automatically</source>
+        <translation>Αυτόματος έλεγχος ενημερώσεων</translation>
+    </message>
+    <message>
+        <source>Save as...</source>
+        <translation>Αποθήκευση ως...</translation>
+    </message>
+    <message>
+        <source>Export diary to single plain text file.
+</source>
+        <translation>Εξαγωγή ημερολογίου σε ενιαίο αρχείο απλού κειμένου.
+</translation>
+    </message>
+    <message>
+        <source>Override default font</source>
+        <translation>Παράκαμψη προεπιλεγμένης γραμματοσειράς</translation>
+    </message>
+    <message>
+        <source>Selected diary</source>
+        <translation>Επιλεγμένο ημερολόγιο</translation>
+    </message>
+    <message>
+        <source>Enable backup</source>
+        <translation>Ενεργοποίηση αντιγράφων ασφαλείας</translation>
+    </message>
+    <message>
+        <source>Datetime:</source>
+        <translation>Ημερομηνία / Ώρα:</translation>
+    </message>
+    <message>
+        <source>Statistics</source>
+        <translation>Στατιστικά</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Εμφάνιση</translation>
+    </message>
+    <message>
+        <source>Default:</source>
+        <translation>Προεπιλογή:</translation>
+    </message>
+    <message>
+        <source>Open output file</source>
+        <translation>Άνοιγμα αρχείου εξόδου</translation>
+    </message>
+    <message>
+        <source>Extend background of titlebar to toolbar</source>
+        <translation>Επέκταση φόντου γραμμής τίτλου στη γραμμή εργαλείων</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ρυθμίσεις</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Γενικά</translation>
+    </message>
+    <message>
+        <source>All diary</source>
+        <translation>Όλο το ημερολόγιο</translation>
+    </message>
+    <message>
+        <source>Preview lines:</source>
+        <translation>Γραμμές προεπισκόπησης:</translation>
+    </message>
+</context>
+<context>
+    <name>Errors</name>
+    <message>
+        <source>Multiple access error</source>
+        <translation>Σφάλμα πολλαπλής πρόσβασης</translation>
+    </message>
+    <message>
+        <source>Failed to access database</source>
+        <translation>Απέτυχε η πρόσβαση στη βάση δεδομένων</translation>
+    </message>
+    <message>
+        <source>Failed to access file</source>
+        <translation>Απέτυχε η πρόσβαση στο αρχείο</translation>
+    </message>
+    <message>
+        <source>This diary book is already open.</source>
+        <translation>Αυτό το βιβλίο ημερολογίου είναι ήδη ανοιχτό.</translation>
+    </message>
+    <message>
+        <source>SQLite3: %s.
+
+Please check database path(have permission?). If it&apos;s corrupt, you may have to recover this file by hand or restore from backups.</source>
+        <translation>SQLite3: %s.
+
+Παρακαλώ ελέγξτε τη διαδρομή της βάσης δεδομένων (έχει δικαιώματα;). Αν είναι κατεστραμμένο, μπορεί να χρειαστεί να ανακτήσετε αυτό το αρχείο χειροκίνητα ή να το επαναφέρετε από αντίγραφα ασφαλείας.</translation>
+    </message>
+</context>
+<context>
+    <name>editor</name>
+    <message>
+        <source>Click to edit</source>
+        <translation>Κάντε κλικ για επεξεργασία</translation>
+    </message>
+    <message>
+        <source>Tags separated by space</source>
+        <translation>Ετικέτες χωρισμένες με κενά</translation>
+    </message>
+    <message>
+        <source>Click to turn off read-only mode</source>
+        <translation>Κάντε κλικ για απενεργοποίηση της λειτουργίας μόνο για ανάγνωση</translation>
+    </message>
+</context>
+<context>
+    <name>DateTimeDialog</name>
+    <message>
+        <source>Edit datetime</source>
+        <translation>Επεξεργασία ημερομηνίας / ώρας</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Άκυρο</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Επαναφορά</translation>
+    </message>
+</context>
+<context>
+    <name>HeatMap</name>
+    <message>
+        <source>(characters)</source>
+        <translation>(χαρακτήρες)</translation>
+    </message>
+</context>
+<context>
+    <name>SearchBox</name>
+    <message>
+        <source>Search</source>
+        <translation>Αναζήτηση</translation>
+    </message>
+</context>
+<context>
+    <name>Editor</name>
+    <message>
+        <source>New Diary</source>
+        <translation>Νέο ημερολόγιο</translation>
+    </message>
+</context>
+</TS>
